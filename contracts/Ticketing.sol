702f3c371839cc1b56b00cb486ec724e2903c09a
Ewennn <ewen.bosquet@gmail.com>
2026-01-21 19:16:28 +0100

    feat/ Ticket has metadata and can be rely to his owner

--- contracts/Ticketing.sol
+++ contracts/Ticketing.sol
@@ -4,11 +4,31 @@ pragma solidity ^0.8.28;
 contract Ticketing {
     string public eventName;
 
+    struct Ticket {
+        uint256 id;
+        address owner;
+        bool used;
+    }
+
+    uint256 public ticketCount;
+    mapping(uint256 => Ticket) public tickets;
+
     constructor() {
         eventName = "Ticketing Dapp";
+        ticketCount = 0;
     }
 
     function getEventName() public view returns (string memory) {
         return eventName;
+    }
+
+    function createTicket() public {
+        ticketCount += 1;
+
+        tickets[ticketCount] = Ticket({
+            id: ticketCount,
+            owner: msg.sender,
+            used: false
+        });
     }
 }
--- test/Ticketing.ts
+++ test/Ticketing.ts
@@ -9,4 +9,18 @@ describe('Ticketing contract', () => {
 
         expect(await ticketing.getEventName()).to.equal("Ticketing Dapp");
     })
+
+    it('Should create a ticket and assign it to the caller', async () => {
+        const Ticketing = await ethers.getContractFactory('Ticketing');
+        const ticketing = await Ticketing.deploy();
+
+        const [owner] = await ethers.getSigners();
+
+        await ticketing.createTicket();
+        const ticket = await ticketing.tickets(1);
+
+        expect(ticket.id).to.equal(1);
+        expect(ticket.owner).to.equal(owner.address);
+        expect
+    })
 });
